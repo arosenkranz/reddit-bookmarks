70f029b543f66b0e615396f48e875b060bebe119
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2019-05-13 20:02:44 -0400

    fixing route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 // use this file to collect ALL routes (api and HTML) and package them
 const router = require('express').Router();
-const apiRoutes = require('./api');
 const path = require('path');
+const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+router.use((req, res) => res.sendFile(path.join(process.cwd(), 'public', 'index.html')));
 
 module.exports = router;
